b7563befbe2743b475d2208c7038f64e0455d796
Mus4ng <nikinhomatheus@hotmail.com>
2022-06-25 01:12:23 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['TT Norms Pro'],
+      },
+
+      fontWeight: {
+        'hairline': 100,
+        'extra-light': 100,
+        'thin': 200,
+        'light': 300,
+        'normal': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+        'extra-bold': 800,
+        'black': 900,
+      },
+
+      fontSize:{
+        'title': ['14.4px', {letterSpacing: '4px'}],
+        'button': ['13px', {letterSpacing: '2px'}],
+        'nav': ['20px', {letterSpacing: '2px'}]
+      },
+
+      backgroundImage: {
+        'welcome': "url('img/bg-welcome.svg')",
+      }
+    },
+  },
+  plugins: [],
+}
